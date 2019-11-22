347641e57a56a5ca3f7414240d4d9cb4e7005413
LRV2K1 <l.r.verhage@hetnet.nl>
2019-11-22 09:12:41 +0100

    V1.1

--- 2.5D Template/GameObjects/LevelGrid.cs
+++ 2.5D Template/GameObjects/LevelGrid.cs
@@ -86,7 +86,6 @@ class LevelGrid : GameObjectGrid
                 int y = z - x;
                 if (x >= grid.GetLength(0) || y >= grid.GetLength(1) || !camera.OnScreen(AnchorPosition(x, y)))
                 {
-                    Console.WriteLine("hello + ph");
                     continue;
                 }
                 Tile tile = GameWorld.GetObject(grid[x, y]) as Tile;
