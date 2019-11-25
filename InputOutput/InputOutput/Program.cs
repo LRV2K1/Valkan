1798889322dc70954791d9457eb6132dd31a82f3
Ruzutsas <Ruzutsas@users.noreply.github.com>
2019-11-25 01:15:43 +0100

    File I/O end
    Working File I/O

--- InputOutput/InputOutput/Program.cs
+++ InputOutput/InputOutput/Program.cs
@@ -1,25 +1,21 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 class IO
 {
     static void Main(string[] args)
-    {        
-        ReadFile(); 
+    {
         ChangeStats();
         ReadFile();
-        Console.ReadLine();        
+        Console.ReadLine();
     }
 
     static void ReadFile()
     {
-        string[] lines;      
+        string[] lines;
         var list = new List<string>();
-        FileStream CharacterStats = new FileStream(@"D:\Uni\InputOutput\Stats.txt", FileMode.Open);
+        FileStream CharacterStats = new FileStream(@"\IO\InputOutput\Stats.txt", FileMode.Open);
         using (var r = new StreamReader(CharacterStats))
         {
             string line;
@@ -29,20 +25,23 @@ class IO
             }
         }
         lines = list.ToArray();
-        for (int i = 0; i < lines.Length; i ++)
-        {           
+        for (int i = 0; i < lines.Length; i++)
+        {
             switch (i)
             {
                 case 0:
                     Console.WriteLine("Name: " + lines[i]);
                     break;
                 case 1:
-                    Console.WriteLine("Strength: " + lines[i]);
+                    Console.WriteLine("HP: " + lines[i]);
                     break;
                 case 2:
-                    Console.WriteLine("Magic " + lines[i]);
+                    Console.WriteLine("Strength: " + lines[i]);
                     break;
                 case 3:
+                    Console.WriteLine("Magic: " + lines[i]);
+                    break;
+                case 4:
                     Console.WriteLine("Speed: " + lines[i]);
                     break;
             }
@@ -51,11 +50,19 @@ class IO
 
     static void ChangeStats()
     {
-        string input;
-        input = Console.ReadLine();
+        string newName;
+        int newHP;
+        int newStrength;
+        int newMagic;
+        int newSpeed;
+        newName = "Valkan";
+        newHP = 100;
+        newStrength = 15;
+        newMagic = 5;
+        newSpeed = 7;
         string[] lines;
         var list = new List<string>();
-        FileStream CharacterStats = new FileStream(@"D:\Uni\InputOutput\Stats.txt", FileMode.Open);
+        FileStream CharacterStats = new FileStream(@"\IO\InputOutput\Stats.txt", FileMode.Open);
         using (var r = new StreamReader(CharacterStats))
         {
             string line;
@@ -65,9 +72,17 @@ class IO
             }
         }
         lines = list.ToArray();
-        lines[0] = lines[0].Replace(lines[0], input);
-        StreamWriter writer = new StreamWriter(File.OpenWrite(@"D:\Uni\InputOutput\Stats.txt"));
+        StreamWriter writer = new StreamWriter(File.OpenWrite(@"\IO\InputOutput\Stats.txt"));
+        lines[0] = lines[0].Replace(lines[0], newName);
         writer.Write(lines[0]);
+        lines[1] = lines[1].Replace(lines[1], Environment.NewLine + newHP);
+        writer.Write(lines[1]);
+        lines[2] = lines[2].Replace(lines[2], Environment.NewLine + newStrength);
+        writer.Write(lines[2]);
+        lines[3] = lines[3].Replace(lines[3], Environment.NewLine + newMagic);
+        writer.Write(lines[3]);
+        lines[4] = lines[4].Replace(lines[4], Environment.NewLine + newSpeed);
+        writer.Write(lines[4]);
         writer.Close();
     }
 }
