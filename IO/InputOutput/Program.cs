e251f795ce6a9f8f0814e6b674d13fc8874b9af6
Ruzutsas <Ruzutsas@users.noreply.github.com>
2019-11-25 02:59:26 +0100

    Moved Stats.txt to \PlayerStats\

--- IO/InputOutput/Program.cs
+++ IO/InputOutput/Program.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 class IO
 {
-    public static string FilePath = "Stats.txt";   
+    public static string FilePath = @"\Users\leung\Documents\GitHub\Valkan\IO\InputOutput\PlayerStats\Stats.txt";   
     static void Main(string[] args)
     {
         ChangeStats();
@@ -40,7 +40,7 @@ class IO
                     Console.WriteLine("Strength: " + lines[i]);
                     break;
                 case 3:
-                    Console.WriteLine("Magic: " + lines[i]);
+                    Console.WriteLine("Range: " + lines[i]);
                     break;
                 case 4:
                     Console.WriteLine("Speed: " + lines[i]);
@@ -54,12 +54,12 @@ class IO
         string newName;
         int newHP;
         int newStrength;
-        int newMagic;
+        int newRange;
         int newSpeed;
         newName = "Valkan";
         newHP = 100;
         newStrength = 15;
-        newMagic = 5;
+        newRange = 5;
         newSpeed = 7;
         string[] lines;
         var list = new List<string>();
@@ -75,14 +75,14 @@ class IO
         lines = list.ToArray();
         StreamWriter writer = new StreamWriter(File.OpenWrite(FilePath));
         lines[0] = lines[0].Replace(lines[0], newName);
-        writer.Write(lines[0]);
         lines[1] = lines[1].Replace(lines[1], Environment.NewLine + newHP);
-        writer.Write(lines[1]);
         lines[2] = lines[2].Replace(lines[2], Environment.NewLine + newStrength);
-        writer.Write(lines[2]);
-        lines[3] = lines[3].Replace(lines[3], Environment.NewLine + newMagic);
-        writer.Write(lines[3]);
+        lines[3] = lines[3].Replace(lines[3], Environment.NewLine + newRange);
         lines[4] = lines[4].Replace(lines[4], Environment.NewLine + newSpeed);
+        writer.Write(lines[0]);
+        writer.Write(lines[1]);
+        writer.Write(lines[2]);
+        writer.Write(lines[3]);
         writer.Write(lines[4]);
         writer.Close();
     }
