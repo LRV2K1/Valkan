f9de6017b67481d9429ad2ef28029444f61d45b9
LRV2K1 <l.r.verhage@hetnet.nl>
2019-12-06 16:05:32 +0100

    Test world

--- 2.5D Template/GameObjects/Skills/Dodge.cs
+++ 2.5D Template/GameObjects/Skills/Dodge.cs
@@ -11,7 +11,7 @@ class Dodge : TertairySkill
 {
     protected float speed;
     protected float runtime;
-    public Dodge(string assetname, float time = 1f, float speed = 2, float runtime = 0.2f)
+    public Dodge(string assetname, float time = 1f, float speed = 2f, float runtime = 0.2f)
         : base(assetname, time)
     {
         this.speed = speed;
