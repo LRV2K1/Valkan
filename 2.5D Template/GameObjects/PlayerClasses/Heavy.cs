dcb0f2b09a960472c375f8e2ae048d33fe52d565
Ruzutsas <Ruzutsas@users.noreply.github.com>
2019-12-07 23:45:09 +0100

    Heavy class

---
+++ 2.5D Template/GameObjects/PlayerClasses/Heavy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Heavy : Player
+{
+    public Heavy()
+    {
+        maxhealth = 20;
+        maxstamina = 5;
+    }
+}
+
