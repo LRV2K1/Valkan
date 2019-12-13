47fc473469129668a2237ff2da52fbf9c18d4de6
Ruzutsas <Ruzutsas@users.noreply.github.com>
2019-12-13 16:15:05 +0100

    I/O

--- 2.5D Template/GameObjects/Entities/Player/Player.cs
+++ 2.5D Template/GameObjects/Entities/Player/Player.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework;
+using System.IO;
+using System.Security.Cryptography;
 using Microsoft.Xna.Framework.Graphics;
 
 struct SpeedMultiplier
@@ -25,6 +27,9 @@ partial class Player : Entity
     protected bool selected;
     protected int health, stamina;
     protected int maxhealth, maxstamina;
+    protected string name, job;
+    protected int playerlevel, playerEXP, EXPThreshold;
+    public int playerID;
     protected Skill skill1, skill3;
     protected Block skill2;
     protected float staminatimer, staminatimerreset, addstaminatimer, addstaminatimerreset;
@@ -37,15 +42,24 @@ partial class Player : Entity
 
     protected List<SpeedMultiplier> speedMultipliers;
 
+    private static string hash = "_+*/(&!*";
+    public static string EncryptedText;
+
     public Player()
         : base(30, 20, 2, "player")
     {
+        name = "Valkan";
+        job = "Light";
+        playerID = 1;
+        playerlevel = 1;
+        playerEXP = 1;
         maxhealth = 10;
         maxstamina = 100;
         health = maxhealth;
         stamina = maxstamina;
         staminatimerreset = 1f;
         addstaminatimerreset = 0.02f;
+        EXPThreshold = 5;
 
         dead = false;
         die = false;
@@ -96,7 +110,6 @@ partial class Player : Entity
 
             RegenStamina(gameTime);
         }
-
         base.Update(gameTime);
     }
 
@@ -125,6 +138,11 @@ partial class Player : Entity
         else if (inputHelper.IsKeyDown(Keys.S))
         {
             direction.Y = 1;
+        }
+        if (inputHelper.KeyPressed(Keys.Q))
+        {
+            WriteStats();
+            ReadStats();
         }
 
         float totalDir = (float)Math.Sqrt(direction.X * direction.X + direction.Y * direction.Y);
@@ -179,5 +197,87 @@ partial class Player : Entity
     public void AddSpeedMultiplier(float time, float multiplier)
     {
         speedMultipliers.Add(new SpeedMultiplier(multiplier, time));
+    }
+
+    public void LevelUp()
+    {
+        if (playerEXP > EXPThreshold)
+        {
+            playerlevel++;
+            playerEXP = playerEXP - EXPThreshold;
+            EXPThreshold = EXPThreshold * 2;
+        }
+        WriteStats();
+    }
+
+    public virtual void WriteStats()                        //Saves the player stats in a text file
+    {
+        string statpath = "Content/PlayerStats/Stats.txt";
+        string[] lines;
+        lines = new string[7];
+        StreamWriter writer = new StreamWriter(statpath);
+        lines[0] = Encrypt(name);
+        lines[1] = Encrypt(job);
+        lines[2] = Encrypt(playerID.ToString());
+        lines[3] = Encrypt(playerlevel.ToString());
+        lines[4] = Encrypt(playerEXP.ToString());
+        lines[5] = Encrypt(maxhealth.ToString());
+        lines[6] = Encrypt(maxstamina.ToString());
+        for (int i = 0; i < lines.Length; i++)
+        {
+            writer.WriteLine(lines[i]);
+            System.Diagnostics.Debug.WriteLine(lines[i]);
+        }
+        writer.Close();
+    }
+
+    public void ReadStats()                                //Reads the player stats in a text file
+    {
+        string statspath = "Content/PlayerStats/Stats.txt";
+        StreamReader streamReader = new StreamReader(statspath);
+        List<string> lines = new List<string>();
+        string line = streamReader.ReadLine();
+        while (line != null)
+        {
+            lines.Add(line);
+            line = streamReader.ReadLine();
+        }
+        for (int i = 0; i < lines.Count; i++)
+        {
+            lines[i] = Decrypt(lines[i]);
+            System.Diagnostics.Debug.WriteLine(lines[i]);
+        }
+        streamReader.Close();
+    }
+
+    public static string Encrypt(string input)
+    {
+        byte[] file = UTF8Encoding.UTF8.GetBytes(input);
+        using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+        {
+            byte[] key = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+            using (TripleDESCryptoServiceProvider trip = new TripleDESCryptoServiceProvider() { Key = key, Mode = CipherMode.ECB })
+            {
+                ICryptoTransform tr = trip.CreateEncryptor();
+                byte[] results = tr.TransformFinalBlock(file, 0, file.Length);
+                EncryptedText = Convert.ToBase64String(results, 0, results.Length);
+                return Convert.ToBase64String(results, 0, results.Length);
+            }
+        }
+    }
+
+    public static string Decrypt(string input)
+    {
+        byte[] file = Convert.FromBase64String(input);
+        using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+        {
+            byte[] key = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+            using (TripleDESCryptoServiceProvider trip = new TripleDESCryptoServiceProvider() { Key = key, Mode = CipherMode.ECB })
+            {
+                ICryptoTransform tr = trip.CreateDecryptor();
+                byte[] results = tr.TransformFinalBlock(file, 0, file.Length);
+                return UTF8Encoding.UTF8.GetString(results);
+            }
+        }
     }
 }
