5e6f0d7db52dfa17acb25ed864f37b90cd49c7b8
Joeri Dukker <joeridukker3@gmail.com>
2020-01-17 21:51:37 +0100

    3

--- 2.5D Template/States/PortSelectionState.cs
+++ 2.5D Template/States/PortSelectionState.cs
@@ -63,6 +63,7 @@ class PortSelectionState : GameObjectLibrary
             if (buttonList[i].Pressed)
             {
                 MultiplayerManager.Connect(9999);
+                MultiplayerManager.party.Send("Join", 9999);
                 // Connect with ipList[i] (dit is het ip die connection heeft gemaakt in string vorm)
                 //Voor nu heb ik de transition naar ClientSelectionState maar die moet absoluut weg uiteindelijk.
                 GameEnvironment.ScreenFade.TransitionToScene("clientSelectionState", 5);
