0032329b58f96ed5b6b103a3463ada0e0ad4e5a1
LRV2K1 <l.r.verhage@hetnet.nl>
2020-01-28 12:31:54 +0100

    Revert "optimizing"
    This reverts commit b8b92c2ade004c4ed758f7b0de83f61e1b0cdd6a.

--- 2.5D Template/GameManagment/Multp/ConnectionParty.cs
+++ 2.5D Template/GameManagment/Multp/ConnectionParty.cs
@@ -42,6 +42,7 @@ public class ConnectionParty : Connection
                     level.DistributeData(data);
                     timer++;
                 }
+                Thread.Sleep(10);
             }
             ar_ = udpclient.BeginReceive(Receive, new object()); //repeat
         }
